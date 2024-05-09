2e18fcecf0423b80623ea7e8e75726b1cc70f998
Thomas Fogarty <thomas.fogarty2429@gmail.com>
2024-05-09 19:58:29 +0100

    create post modal added

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -11,12 +11,26 @@ class CreatePost extends Component
 
     public $post_content;
     public $user_id;
+    public $user_name;
+    public $post_modal_open = false;
 
     // @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
 
+    public function closeModal() 
+    {
+        $this->post_modal_open = false;
+    }
+
+    public function openModal() 
+    {
+        $this->post_modal_open = true;
+    }
+
     public function mount() 
     {
-        $this->user_id = Auth::id();
+        $user = Auth::user();
+        $this->user_id = $user->id;
+        $this->user_name = $user->name;
     }
 
     public function createPost() 
@@ -25,6 +39,8 @@ class CreatePost extends Component
             'content' => $this->post_content,
             'user_id' => $this->user_id,
         ]);
+        
+        $this->closeModal();
     }
 
     public function render()
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,9 +1,22 @@
-<form wire:submit='createPost'>
+<div>
+    <button class="bg-red-400 p-2 m-4 rounded" wire:click='openModal'>Make post</button>
+    @if($post_modal_open)
+    <div>
+        <x-modal class="h-dvh">
+            <div class="h-60">
+                <form wire:submit='createPost' class="bg-red-500">
     
-        <img src="" alt="">
-        <h3>Name here</h3>
-   
-        <input type="text" wire:model='post_content'>
+                    <img src="" alt="">
+                    <h3>{{ $user_name }}</h3>
     
-    <button type="submit">Post</button>
-</form>
+                    <input type="text" wire:model='post_content'>
+    
+                    <button type="submit">Post</button>
+                    
+                </form>
+                <button wire:click='closeModal' class="m-5">Close</button>
+            </div>
+        </x-modal>
+    </div>
+    @endif
+</div>
